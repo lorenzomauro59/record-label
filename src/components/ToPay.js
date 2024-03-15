3f0c7ff13f826fe7c3de86122609bd4b88625e58
Lorenzo Mauro <lorenzomauro@MacBook-Air-de-Lorenzo.local>
2024-03-15 16:53:23 -0300

    change

--- src/components/ToPay.js
+++ src/components/ToPay.js
@@ -15,32 +15,8 @@ export default function toPay() {
         <Image src="/FLYER.png" alt="flyer" layout="fill" objectFit="cover" />
       </div>
 
-      {/* Primer botón */}
-      <div className="flex items-center justify-center mb-4 animate-slideInLeft">
-        <p className="text-sm mr-4">1 x $3000</p>
-        <a
-          href="https://mpago.la/1Py2cnp"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="custom-mp-button ml-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Pagar
-        </a>
-      </div>
-
       <div>
-        <p className="text-sm p-6 text-center">
-          Al realizar el pago, no olvides enviar el comprobante a nuestro{" "}
-          <a
-            href="https://www.instagram.com/holobeatrec"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-blue-500 hover:underline"
-          >
-            Instagram
-          </a>{" "}
-          😊
-        </p>
+        <p className="text-sm p-6 text-center">Anticipadas agotadas! ❌</p>
       </div>
     </div>
   );
