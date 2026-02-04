d784dc878bec1224d50cec0353d1713afecafdfd
lorenzomauro59 <lorenzomauro59@gmail.com>
2026-02-04 19:41:25 -0300

    change

--- src/components/MixSeries.js
+++ src/components/MixSeries.js
@@ -8,7 +8,7 @@ export default function MixSeries() {
     threshold: 0.1,
   });
 
-  const youtubeEmbedCode = `<iframe width="100%" height="400" src="https://www.youtube.com/embed/aUfadJ09eGM?si=lVPjICjx0m6ihIRi" title="Holobeat Mix Series" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen style="border-radius: 12px;"></iframe>`;
+  const youtubeEmbedCode = `<iframe width="100%" height="400" src="https://www.youtube.com/embed/fNKeroBjLLk?si=poC2A_GrSapKlVVf" title="Holobeat Mix Series" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen style="border-radius: 12px;"></iframe>`;
 
   return (
     <section id="mix-series" className="w-full bg-white pb-20 px-4">
